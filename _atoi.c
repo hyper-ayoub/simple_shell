8c13af5f0af58793fb5644c79780048004c65922
hyperayoub <bouagnaayoub@gmail.com>
2023-09-23 06:48:41 -0700

    shell

--- _atoi.c
+++ _atoi.c
@@ -47,29 +47,30 @@ int _isalpha(int c)
 
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
+int i, sign = 1, flag = 0, output;
+unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
+for (i = 0; s[i] != '\0' && flag != 2; i++)
+{
+if (s[i] == '-')
+sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-
-	if (sign == -1)
-output = -result;
-	else
-		output = result;
-
-	return (output);
+if (s[i] >= '0' && s[i] <= '9')
+{
+flag = 1;
+result *= 10;
+result += (s[i] - '0');
+}
+else if (flag == 1)
+{
+flag = 2;
+}
 }
 
+if (sign == -1)
+output = -result;
+else
+output = result;
+
+return (output);
+}
